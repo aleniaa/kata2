9ff31378feb759da3af53fb9ff699c7f219371e3
milen <milen@10.195.253.186>
2019-10-09 13:39:38 +0200

    complete

--- src/main/java/Histogram.java
+++ src/main/java/Histogram.java
@@ -1,17 +1,26 @@
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class Histogram implements Iterable<Integer> {
 
         private final Map<Integer,Integer> counts = new HashMap<>();
 
-    public Histogram() {
+    public Histogram(int... values) {
+        for (int value : values)
+            counts.put(value, counts.getOrDefault(value, 0)+1);
     }
 
     
     
     
-    {
+    @Override
+    public Iterator<Integer> iterator(){
+        return counts.keySet().iterator();
+    }
     
+    public int getCount(int value){
+        return counts.get(value);
+    }
     
 }
