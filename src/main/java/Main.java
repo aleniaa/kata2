16a6c821c9fae652aba4249b3e066f7c6d4e7b36
milen <milen@10.195.253.186>
2019-10-09 13:52:24 +0200

    complete1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,8 @@
 
 public class Main {
-    
-    
-    
-    
-    public static void main(String[] args){
-        Histogram histogram= new Histogram(2,3,4,5,6,7,6);
-        for(int value : histogram){
-            System.out.println(value + " " + histogram.getCount(value));
-        }
+
+    public static void main(String[] args) {
+        Histogram histogram = new Histogram(2, 3, 4, 5, 6, 7, 6);
+        new ConsoleHistogramDisplay().show(histogram);
     }
 }
